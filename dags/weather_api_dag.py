006a96f7fff81d1de0608ada0186911d9f29522d
MrSomyong <53037256+MrSomyong@users.noreply.github.com>
2025-02-01 09:15:25 +0000

    Finish Week 02

---
+++ dags/weather_api_dag.py
@@ -0,0 +1,43 @@
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+from airflow.utils import timezone
+
+import requests
+
+
+def _get_weather_data():
+    # API_KEY = "0c6a6c23437b95436c85ddd02524697d"
+    # API_KEY = os.environ.get("WEATHER_API_KEY")
+    API_KEY = Variable.get("weather_api_key")
+
+    payload = {
+        "q": "bangkok",
+        "appid": API_KEY,
+        "units": "metric"
+    }
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    response = requests.get(url, params=payload)
+    print(response.url)
+
+    data = response.json()
+    print(data)
+
+
+with DAG(
+    "weather_api_dag",
+    #schedule="@hourly",
+    schedule="* */3 * * *",
+    start_date=timezone.datetime(2025, 1, 31),
+    tags=["dpu","get_weather_data"]
+):
+    start = EmptyOperator(task_id="start")
+
+    get_weather_data = PythonOperator(
+        task_id="get_weather_data",
+        python_callable=_get_weather_data,
+    )
+
+    end = EmptyOperator(task_id="end")
+
+    start >> get_weather_data >> end
